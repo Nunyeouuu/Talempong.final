80f13a311d9d3e09d354dd47ec005e2a610c4c5e
Nunyeouuu <nunyeouuu@gmail.com>
2022-08-12 18:01:19 +0700

    Menu

--- src/Screen/Menu.js
+++ src/Screen/Menu.js
@@ -1,23 +1,30 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-import { Text, View, Image, StyleSheet, ImageBackground } from "react-native";
-import { Button } from "@rneui/themed";
+import { Text, View, StyleSheet, ImageBackground } from "react-native";
+import { Button, Image } from "@rneui/themed";
 import { image } from "../utils/Constant";
 
 export const Menu = () => {
     return(
         <>
         <View>
-            <View>
+            <View style={{flex:1,}}>
                 <ImageBackground source={image.Background} style={styles.bg}>
-                    <Button 
-                    title={'button'} type={"clear"}>
-                        <Image source={image.Info1} 
-                        style={{
-                        width: 100,
-                        height: 60
-                        }} />
-                    </Button>
+                    <View style={{ justifyContent:"space-around", flexDirection:"row",
+                    width: 650, height: 100, alignSelf: 'center', marginVertical: 175 }}> 
+                        <View>
+                            <Image 
+                            source={image.Info1}
+                            style={styles.button}
+                            onPress={() => console.log(`help`)}/>
+                        </View>
+                        <View>
+                            <Image 
+                            source={image.Info1}
+                            style={styles.button}
+                            onPress={() => console.log(`help`)}/>
+                        </View>
+                    </View>
                 </ImageBackground>
             </View>
         </View>
@@ -29,7 +36,13 @@ const styles = StyleSheet.create ({
     bg: {
         widht: 200, 
         height: 400,
-    }
+    },
+    button: {
+        height: 140,
+        width: 210,
+        alignContent: 'center',
+
+    } 
 })
 
 
