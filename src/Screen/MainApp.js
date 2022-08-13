a91c0e2fdb2dbe819b4a1d9975a7bf44b420b090
Nunyeouuu <nunyeouuu@gmail.com>
2022-08-13 21:43:50 +0700

    add sound

--- src/Screen/MainApp.js
+++ src/Screen/MainApp.js
@@ -1,18 +1,45 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-import { Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Text, TouchableOpacity, View } from "react-native";
+import Sound from "react-native-sound";
+import { sfx } from "../utils/Constant";
+
 
 export const MainApp = () => {
+    const PlaySound = async (FileName) => {
+        const sound = new Sound(FileName, Sound.MAIN_BUNDLE, (error) => {
+            if (error) {
+                console.log(error)
+            }
+
+            const toSecond = 1000;
+
+            const totalTime = sound.getDuration() * toSecond;
+            sound.play((success) => {
+                if (!success) return;
+
+                setTimeout(() => {
+                    sound.release();
+                }, totalTime)
+            })
+        })
+    }
+
     return(
         <>
         <View>
             <View>
-                <Text>bruh</Text>
+                <TouchableOpacity onPress={() => PlaySound(sfx._do)} style={{width: 100, height: 50, backgroundColor:'red'}}/>
+                <TouchableOpacity onPress={() => PlaySound(sfx.re)} style={{width: 100, height: 50, backgroundColor:'red'}}/>
+                <TouchableOpacity onPress={() => PlaySound(sfx.mi)} style={{width: 100, height: 50, backgroundColor:'red'}}/>
+                <TouchableOpacity onPress={() => PlaySound(sfx.fa)} style={{width: 100, height: 50, backgroundColor:'red'}}/>
             </View>
         </View>
         </>
     );
-}
+};
+
+
 
 
 const Stack = createNativeStackNavigator();
--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -2,3 +2,13 @@ export const image = {
     Background: require('../Img/bg.png'),
     Info1: require('../Img/info1.png')
 }
+
+export const sfx = {
+    _do: '_do.wav',
+    re: 'mi.wav',
+    fa: 'fa.wav',
+    sol: 'sol.wav',
+    la: 'la.wav',
+    si: 'si.wav',
+    do1: 'do1.wav',
+}
